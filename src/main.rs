cff26845aadea24371ec3cea58d6f1fe1eed90c5
Antoine Busch <antoine.busch@gmail.com>
2023-10-21 15:25:56 +1100

    git hash-file

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,15 @@
 use std::env;
 #[allow(unused_imports)]
 use std::fs;
-use std::io::{self, BufRead, BufReader};
+use std::io::{self, BufRead, BufReader, Write};
 use std::path::PathBuf;
 
+use anyhow::bail;
 use anyhow::{Context, Result};
 use clap::Parser;
 use clap::Subcommand;
+use flate2::Compression;
+use sha1::{Digest, Sha1};
 
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
@@ -24,6 +27,10 @@ pub enum Commands {
         #[arg(short = 'p', value_name = "blob_sha")]
         sha: String,
     },
+    HashFile {
+        #[arg(short = 'w', value_name = "file")]
+        file: String,
+    },
 }
 
 fn main() -> Result<()> {
@@ -31,6 +38,7 @@ fn main() -> Result<()> {
     match args.command {
         Commands::Init => git_init()?,
         Commands::CatFile { sha } => git_cat_file(sha)?,
+        Commands::HashFile { file } => git_hash_file(file)?,
     }
 
     Ok(())
@@ -64,6 +72,57 @@ pub fn git_cat_file(sha: String) -> Result<()> {
     assert!(buf.starts_with(b"blob "), "Invalid blob file");
     // Write content of the blob to stdout
     io::copy(&mut reader, &mut stdout).context("Writing blob content to stdout")?;
+
+    Ok(())
+}
+
+pub fn git_hash_file(file: String) -> Result<()> {
+    let file_content = fs::read(file).context("Reading file to hash")?;
+    let file_size = file_content.len();
+
+    let mut hasher = Sha1::new();
+    hasher.update(&file_content);
+    let result = hasher.finalize();
+    let sha1 = hex::encode(result);
+    println!("sha1 = {sha1}");
+
+    let (dir_name, file_name) = sha1.split_at(2);
+    let blob_dir: PathBuf = [".git", "objects", dir_name].iter().collect();
+    match blob_dir.try_exists() {
+        // dir already exits
+        Ok(true) => (),
+        // dir doesn't exist: created it
+        Ok(false) => {
+            println!("Creating blob directory {}", blob_dir.display());
+            fs::create_dir(&blob_dir).context("Creating blob directory")?;
+        }
+        Err(e) => bail!(e),
+    }
+
+    let blob_file_name = blob_dir.join(file_name);
+    // Create blob file
+    let mut blob_file = fs::File::options()
+        .create(true)
+        .write(true)
+        .open(blob_file_name)
+        .context("Creating blob file")?;
+    // Wrap blob file in zlib encoder
+    let mut compressed_content =
+        flate2::write::ZlibEncoder::new(&mut blob_file, Compression::fast());
+
+    let header = format!("blob {file_size}\0");
+    // Write header
+    compressed_content
+        .write_all(header.as_bytes())
+        .context("Writing blob header")?;
+    // Write content
+    compressed_content
+        .write_all(&file_content)
+        .context("Writing blob content")?;
+    // Finalize stream
+    compressed_content
+        .finish()
+        .context("Finalizing compressed stream")?;
 
     Ok(())
 }
